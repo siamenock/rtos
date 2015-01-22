9d7a10ad7921968c1dc6cb761262611b595b5774
Yang Youseok <youseok@gurum.cc>
2015-01-22 13:16:08 +0900

    Macro for flags_offset added
    * Flags_offset in IP header field needs to be separated
    * Bit reversing problem fixed by ip_flags_offset macro

--- lib/core/include/net/ip.h
+++ lib/core/include/net/ip.h
@@ -13,6 +13,8 @@
 #define IP_PROTOCOL_ESP         0x32
 #define IP_PROTOCOL_AH          0x33
 
+#define ip_flags_offset(flag, offset) endian16((((uint16_t)(flag) & 0x07) << 13) | ((uint16_t)(offset) & 0x1fff))
+
 typedef struct {
 	uint8_t		ihl: 4;
 	uint8_t		version: 4;
@@ -20,7 +22,7 @@ typedef struct {
 	uint8_t		dscp: 6;
 	uint16_t	length;
 	uint16_t	id;
-	uint16_t	flags_offset;
+	uint16_t 	flags_offset;
 	uint8_t		ttl;
 	uint8_t		protocol;
 	uint16_t	checksum;
