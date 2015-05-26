cea9a60d8447031ea7ec79a64f0f03a74608a1cc
Semih Kim <semih@gurum.cc>
2015-05-26 22:03:39 +0900

    Basic documentation of Core library
    * Mainpage
    * gmalloc

--- lib/core/include/gmalloc.h
+++ lib/core/include/gmalloc.h
@@ -3,9 +3,24 @@
 
 #include <stddef.h>
 
+/**
+ * Allocate memory chunk from global area
+ */
 void *gmalloc(size_t size);
+
+/**
+ * Free memory chunk to global area
+ */
 void gfree(void *ptr);
+
+/**
+ * Allocate and clear memory chunk from global area
+ */
 void *gcalloc(size_t nmemb, size_t size);
+
+/**
+ * Reallocate memory chunk from global area
+ */
 void *grealloc(void *ptr, size_t size);
 
 #endif /* __GMALLOC_H__ */
