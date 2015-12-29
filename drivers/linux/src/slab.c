98f919984c6ebdee0bd6d2a3da18fba8d45979fe
sungho kim <pugboy23@gmail.com>
2015-12-29 18:34:41 +0900

    slab is modified
    * kmalloc address is aligned

--- drivers/linux/src/slab.c
+++ drivers/linux/src/slab.c
@@ -2,43 +2,180 @@
 #include <string.h>
 #include <gmalloc.h>
 
+#define PAGE_SHIFT 8
 // PacketNgin use unseparted kernel space memory 
 void* kmalloc(size_t size, gfp_t flags) {
-	return gmalloc(size);	
+	uint64_t align;
+
+	if(size <= 0)
+		return NULL;
+
+	/* Check the size is whether to be square of 2 or not */
+	if((size & (size - 1)) != 0)
+		align = 1 << PAGE_SHIFT; // default is 4kb alignment 	
+	else
+		align = size;
+
+	void *ptr;
+	void *p = gmalloc(size + align - 1 + sizeof(void*));
+
+	if(!p)
+		return NULL;
+
+	/* Address of the aligned memory according to the align parameter*/
+	ptr = (void*) (((uint64_t)p + sizeof(void*) + align -1) & ~(align-1));
+	/* store the address of the malloc() above
+	 * at the beginning of our total memory area.
+	 * You can also use *((void **)ptr-1) = p
+	 * instead of the one below.
+	 */
+	*((void**)((uint64_t)ptr - sizeof(void*))) = p;
+	/* Return the address of aligned memory */
+
+	return ptr; 
 }
 
 void kfree(const void* objp) {
-	if(objp)
-		gfree((void*)objp);
+	void* ptr = *((void**)((uint64_t)objp - sizeof(void*)));
+
+	if(ptr)
+		gfree(ptr);
 }
 
 void* kzalloc(size_t size, gfp_t flags) {
-	void* ptr = gmalloc(size);
-	bzero(ptr, size);
-	
-	return ptr;
+	uint64_t align; 
+	if(size <= 0)
+		return NULL;
+
+	/* Check the size is whether to be square of 2 or not */
+	if((size & (size - 1)) != 0)
+		align = 1 << PAGE_SHIFT; // default is 4kb alignment 	
+	else
+		align = size;
+
+	void *ptr;
+	void *p = gmalloc(size + align - 1 + sizeof(void*));
+
+	if(!p)
+		return NULL;
+	bzero(p, size + align - 1 + sizeof(void*));
+
+	/* Address of the aligned memory according to the align parameter*/
+	ptr = (void*) (((uint64_t)p + sizeof(void*) + align -1) & ~(align-1));
+	/* store the address of the malloc() above
+	 * at the beginning of our total memory area.
+	 * You can also use *((void **)ptr-1) = p
+	 * instead of the one below.
+	 */
+	*((void**)((uint64_t)ptr - sizeof(void*))) = p;
+	/* Return the address of aligned memory */
+
+	return ptr; 
 }
 
 void* kcalloc(size_t n, size_t size, gfp_t flags) {
-	void* ptr = gmalloc(size * n);
-	bzero(ptr, size * n);
+	uint64_t align; 
+
+	if(size <= 0)
+		return NULL;
+
+	if(n <= 0)
+		return NULL;
+
+	size *= n;
+	/* Check the size is whether to be square of 2 or not */
+	if((size & (size - 1)) != 0)
+		align = 1 << PAGE_SHIFT; // default is 4kb alignment 	
+	else
+		align = size;
+
+	void *ptr;
+	void *p = gmalloc(size + align - 1 + sizeof(void*));
+
+	if(!p)
+		return NULL;
+	bzero(p, size + align - 1 + sizeof(void*));
+
+	/* Address of the aligned memory according to the align parameter*/
+	ptr = (void*) (((uint64_t)p + sizeof(void*) + align -1) & ~(align-1));
+	/* store the address of the malloc() above
+	 * at the beginning of our total memory area.
+	 * You can also use *((void **)ptr-1) = p
+	 * instead of the one below.
+	 */
+	*((void**)((uint64_t)ptr - sizeof(void*))) = p;
+	/* Return the address of aligned memory */
+	bzero(ptr, size);
 
 	return ptr;
 }
 
 void* kmalloc_node(size_t size, int flag, int node) {
-	return gmalloc(size);
+	uint64_t align;
+
+	if(size <= 0)
+		return NULL;
+
+	/* Check the size is whether to be square of 2 or not */
+	if((size & (size - 1)) != 0)
+		align = 1 << PAGE_SHIFT; // default is 4kb alignment 	
+	else
+		align = size;
+
+	void *ptr;
+	void *p = gmalloc(size + align - 1 + sizeof(void*));
+
+	if(!p)
+		return NULL;
+
+	/* Address of the aligned memory according to the align parameter*/
+	ptr = (void*) (((uint64_t)p + sizeof(void*) + align -1) & ~(align-1));
+	/* store the address of the malloc() above
+	 * at the beginning of our total memory area.
+	 * You can also use *((void **)ptr-1) = p
+	 * instead of the one below.
+	 */
+	*((void**)((uint64_t)ptr - sizeof(void*))) = p;
+	/* Return the address of aligned memory */
+
+	return ptr; 
 }
 
 void* vzalloc(size_t size) {
-	void* ptr = gmalloc(size);
-	bzero(ptr, size);
+	uint64_t align; 
+	if(size <= 0)
+		return NULL;
 
-	return ptr;
+	/* Check the size is whether to be square of 2 or not */
+	if((size & (size - 1)) != 0)
+		align = 1 << PAGE_SHIFT; // default is 4kb alignment 	
+	else
+		align = size;
+
+	void *ptr;
+	void *p = gmalloc(size + align - 1 + sizeof(void*));
+
+	if(!p)
+		return NULL;
+	bzero(p, size + align - 1 + sizeof(void*));
+
+	/* Address of the aligned memory according to the align parameter*/
+	ptr = (void*) (((uint64_t)p + sizeof(void*) + align -1) & ~(align-1));
+	/* store the address of the malloc() above
+	 * at the beginning of our total memory area.
+	 * You can also use *((void **)ptr-1) = p
+	 * instead of the one below.
+	 */
+	*((void**)((uint64_t)ptr - sizeof(void*))) = p;
+	/* Return the address of aligned memory */
+
+	return ptr; 
 }
 
 void vfree(const void* objp) {
-	if(objp)
-		gfree((void*)objp);
+	void* ptr = *((void**)((uint64_t)objp - sizeof(void*)));
+
+	if(ptr)
+		gfree(ptr);
 }
 
