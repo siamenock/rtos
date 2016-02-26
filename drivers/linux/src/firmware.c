26f4b088bbaf6e7678ae90b775b16ab2f603d107
SanggyuLee <lp31004@gmail.com>
2016-02-26 10:25:18 +0900

    Files in firmware are added in ramdisk
    * Modified firmware.c in linux compatibility layer to use /boot where ramdisk is mounted

--- drivers/linux/src/firmware.c
+++ drivers/linux/src/firmware.c
@@ -1,18 +1,23 @@
 #include <linux/firmware.h>
 #include <linux/printk.h>
+#include <linux/string.h>
 #include <gmalloc.h>
 #include "driver/fs.h"
 
 int request_firmware(const struct firmware **fw, const char *name, PCI_Device *device) {
-	int fd = open(name, "r");
+	char file_name[FILE_MAX_NAME_LEN];
+	strcpy(file_name, "/boot/");
+	strcpy(&file_name[6], name);
+
+	int fd = open(file_name, "r");
 	if(fd < 0) {
-		printf("Unknown firmware: '%s'\n", name);
+		printf("Unknown firmware: '%s'\n", file_name);
 		return -1;
 	}
 
 	uint32_t size = file_size(fd);
 	if(size < 0) {
-		printf("\tCannot get state of file: %s\n", name);
+		printf("\tCannot get state of file: %s\n", file_name);
 		return -2;
 	}
 		
