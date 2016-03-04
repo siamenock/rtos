7e02b46b8de2af445a4f6b0e5fdff8dc2b14755d
Yang yooseok <Youseok@gurum.cc>
2016-03-04 17:23:37 +0900

    Lock code errata fixed
    * Loop was inversed. Inner and outer loop should be changed.

--- lib/core/src/lock.c
+++ lib/core/src/lock.c
@@ -7,13 +7,13 @@ void lock_init(uint8_t volatile* lock) {
 bool cmpxchg(uint8_t volatile* s1, uint8_t s2, uint8_t d);
 
 void lock_lock(uint8_t volatile* lock) {
-	while(!lock_trylock(lock)) {
-		while(cmpxchg(lock, 0, 1));
-	}
+	do {
+		while(lock_trylock(lock));
+	} while(cmpxchg(lock, 0, 1));
 }
 
 bool lock_trylock(uint8_t volatile* lock) {
-	return lock;
+	return *lock;
 }
 
 void lock_unlock(uint8_t volatile* lock) {
