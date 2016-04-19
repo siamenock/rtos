a9c9950a9aa4fa0e318370a9cd30f1e5b85d17f5
Yang Yooseok <youseok@gurum.cc>
2016-04-19 17:59:22 +0900

    Runtime test & shell test completely implemented
    * test.js automatically generates all shell commands to be tested using
    test cases written in case.js.
    * Kernel runtime test parsing logic added to test.js.
    * Make command 'make run option=test' added. It makes UNIX fifo to
    input shell command to PacketNgin RTOS serial console.
    * Junit formatted XML file generated as well as readable testing output messages
    forward to stdout. This XML format is used by Jenkins testing report.

--- test/case.js
+++ test/case.js
@@ -1,5 +1,5 @@
-// Test cases
-var cases = {
+// Test cases related to shell command
+var shell = {
     /* 0.0.431-8fb76c5 */
     version : "\\d+\\.\\d+.\\d+-[a-z0-9]+",
     /* Enabled | Disabled | Not Supported */
@@ -10,11 +10,39 @@ var cases = {
     date    : "(Sun|Mon|Tue|Wed|Thu|Fri|Sat)\\s+(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\\s+\\d{2}\\s+\\d{2}:\\d{2}:\\d{2}\\s+UTC\\s+\\d{4}",
     manager : {
         /* 192.168.100.254 */
-        ip  : "\\d{3}\\.\\d{3}.\\d{3}.\\d{3}",
+        ip      : "\\d{3}\\.\\d{3}.\\d{3}.\\d{3}",
         /* 111 */
-        port : "\\d{1,4}",
+        port    : "\\d{1,4}",
+        /* 192.168.100.200 */
+        gateway : "\\d{3}\\.\\d{3}.\\d{3}.\\d{3}",
+    },
+}
+
+// Test which executed in kernel. 
+var kernel = {
+    test : {
+        /* 
+         * Test case report format 
+         *
+         * [ Test 001 ] test_open
+         * [ PASS     ]
+         * [==========]
+         * [ Test 002 ] test_close
+         * [     FAIL ] ../src/test/file.c:29: file_close should return 0 for success
+         */
+        name        : "\\[ Test \\d+ \\] (.*)",
+        pass        : "\\[ PASS\\s+\\]",
+        fail        : "\\[\\s+FAIL \\] (.*)",
+        /* 
+         * Final report string
+         *
+         * Test Cases: 4  Suceess: 3  Errors: 1 
+         */
+        cases       : "Test Cases: (\\d*)",
+        success     : "Success: (\\d*)",
+        errors      : "Errors: (\\d*)",
     }
 }
 
-module.exports = cases;
-
+exports.shell = shell;
+exports.kernel = kernel;
