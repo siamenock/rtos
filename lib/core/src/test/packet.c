de17a813568b21e240be897735e215cfe0339609
chang-gyu <changgyu@gurum.cc>
2016-09-27 12:01:00 +0900

    Refactoring unit test code of packet
    * Affixing comments in code.
    * The way of making dummy packet into strings is more easier then before.

--- lib/core/src/test/packet.c
+++ lib/core/src/test/packet.c
@@ -11,7 +11,7 @@
 #include <string.h>
 
 #define LENGTH		59
-#define EX_BUF_MAZ	48
+#define EX_BUF_MAZ	48 
 
 static void extract_token(int write_fd, char* str);
 
@@ -23,19 +23,22 @@ static void	packet_dump_func(void** state) {
 	char comp_ch[128];
 	memset(comp_ch, 0, 128);
 
-	// change stdout to write_fd.
+	// Changing stdout to write_fd.
 	dup2(1, stdout_write_fd);
 	dup2(write_fd, 1);
 
+	// Creating dummy packet 
 	Packet* packet = malloc(sizeof(Packet) + LENGTH);
-
 	packet->start = 0;
 	packet->end = LENGTH;
+
 	for(int i = 0; i < LENGTH; i++)
-		*((packet->buffer) + i) = i;
+		*((packet->buffer) + i) = i + 10;
+
+	// Printing dummy packet
 	packet_dump(packet);
 
-	// change stdout_fd to original fd
+	// Change stdout_fd to original fd
 	dup2(stdout_write_fd, 1);
 
 	int index = 0;
@@ -45,21 +48,20 @@ static void	packet_dump_func(void** state) {
 		
 		while(index != LENGTH) {
 			extract_token(read_fd, comp_ch);
+
+			// Extracting dummy packet by 16 bytes
 			char* ex_buf = (char*)malloc(sizeof(char) * EX_BUF_MAZ);
 
-			for(int i = 0; i < (strlen(comp_ch) + 1) / 3; i++) {
-				sprintf(ex_buf + i * 3, "%02x", index++);
+			for(size_t i = 0; i < (strlen(comp_ch) + 1) / 3; i++) {
+				sprintf(ex_buf + i * 3, "%02x", *((packet->buffer) + index++));
 				if((i + 1) % 16 != 0)
 					sprintf(ex_buf + i * 3 + 2, " ");
 				else {
-					sprintf(ex_buf + i * 3 + 2, "\0");
 					break;
 				}
 			}
-
-			if(index % 16 != 0)
-				sprintf(ex_buf + index * 3 + 2, "\0");
-
+			
+			// Comparing rtn of packet_dump func and dummy packet by line
 			assert_string_equal(comp_ch, ex_buf);
 			free(ex_buf);
 		}
@@ -70,6 +72,9 @@ static void	packet_dump_func(void** state) {
 	close(read_fd);
 }
 
+/**
+ * extract_token : This function extracts a single line of string from write_fd.
+ */
 static void extract_token(int write_fd, char* str) {
 	int len = 0;
 	memset(str, 0, 128);
