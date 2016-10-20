89cdbe7d5365f78d006a5563320686bde994ffdf
Yang Yooseok <youseok@gurum.cc>
2016-10-20 14:10:59 +0900

    Initial commit
    * Linux kernel module for PenguinNgin.
    * This kernel module named dispatcher is mapped to PacketNgin manager
    application by one-on-one.
    * Virtio network device driver should be separated. We have to use later
    version of NAPI like busy_poll at that time.

---
+++ dispatcher/dispatcher.h
@@ -0,0 +1,26 @@
+#ifndef __DISPATCHER_H__
+#define __DISPATCHER_H__
+
+#include <linux/types.h> 
+
+#define DISPATCHER			0xAC
+
+#define DISPATCHER_SET_MANAGER		_IOW(DISPATCHER, 0x00, pid_t)
+#define DISPATCHER_UNSET_MANAGER	_IO(DISPATCHER, 0x01)
+
+#define DISPATCHER_REGISTER_NIC		_IO(DISPATCHER, 0x10)
+#define DISPATCHER_UNREGISTER_NIC	_IOW(DISPATCHER, 0x11, void *)
+
+typedef void (*dispatcher_work_fn_t)(void *data);
+
+struct dispatcher_work {
+	struct list_head	node;
+	dispatcher_work_fn_t	fn;
+	void*			data;
+	struct net_device*	dev;
+};
+
+int dispatcher_init(struct net_device *dev);
+void dispatcher_exit(void);
+
+#endif /* __DISPATCHER_H__ */
