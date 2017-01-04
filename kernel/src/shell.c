4a017298a707c86359cfe289cf1600920f8ef811
sungho kim <sungho@gurum.cc>
2017-01-04 11:23:00 +0900

    Command upload has been fixed.
    * Adding root to binary path has been erased.

--- kernel/src/shell.c
+++ kernel/src/shell.c
@@ -974,15 +974,9 @@ static int cmd_upload(int argc, char** argv, void(*callback)(char* result, int e
 	}
 
 	uint32_t vmid = parse_uint32(argv[1]);
-
-	// Attach root directory for full path
-	char file_name[FILE_MAX_NAME_LEN];
-	file_name[0] = '/';
-	strcpy(&file_name[1], argv[2]);
-
-	int fd = open(file_name, "r");
+	int fd = open(argv[2], "r");
 	if(fd < 0) {
-		printf("Cannot open file: %s\n", file_name);
+		printf("Cannot open file: %s\n", argv[2]);
 		return 1;
 	}
 
@@ -991,7 +985,7 @@ static int cmd_upload(int argc, char** argv, void(*callback)(char* result, int e
 	char buf[4096];
 	while((len = read(fd, buf, 4096)) > 0) {
 		if(vm_storage_write(vmid, buf, offset, len) != len) {
-			printf("Upload fail : %s\n", file_name);
+			printf("Upload fail : %s\n", argv[2]);
 			callback("false", -1);
 			return -1;
 		}
