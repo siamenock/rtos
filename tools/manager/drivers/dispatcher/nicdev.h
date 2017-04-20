ba5de9ac33c11c151e0d30b4535528eb5d5e778a
sunghokim <sungho@gurum.cc>
2017-04-20 17:35:44 +0900

    nicdev has been fixed

--- tools/manager/drivers/dispatcher/nicdev.h
+++ tools/manager/drivers/dispatcher/nicdev.h
@@ -1,15 +1,19 @@
 #ifndef __NICDEV_H__
 #define __NICDEV_H__
 
-#include <net/packet.h>
+#include <vnic.h>
 
+#define ETHER_MULTICAST		0Xffffffffffff
+#define MAX_NIC_DEVICE_COUNT	128
 #define MAX_NIC_NAME_LEN	16
+#define MAX_VNIC_COUNT		8
 
 typedef struct {
 	char		name[MAX_NIC_NAME_LEN];
 	uint64_t	mac;
 
-	Map*		vnics;
+	//Map*		vnics; //key mac
+	VNIC*		vnics[MAX_VNIC_COUNT];
 } NICDevice;
 
 enum NICDEV_PROCESS_RESULT {
@@ -17,6 +21,15 @@ enum NICDEV_PROCESS_RESULT {
 	NICDEV_PROCESS_PASS,
 };
 
+int nicdev_init();
+bool nicdev_register(NICDevice* dev);
+NICDevice* nicdev_unregister(char* name);
+NICDevice* nicdev_get(char* name);
+
+bool nicdev_register_vnic(NICDevice* dev, VNIC* vnic);
+VNIC* nicdev_unregister_vnic(NICDevice* dev, VNIC* dst_vnic);
+VNIC* nicdev_update_vnic(NICDevice* dev, VNIC* dst_vnic);
+VNIC* nicdev_get_vnic(NICDevice* dev, VNIC* vnic);
 /**
  * @param dev NIC device
  * @param data data to be sent
@@ -34,6 +47,6 @@ int nicdev_rx(NICDevice* dev, void* data, size_t size);
  * @return number of packets proccessed
  */
 int nicdev_tx(NICDevice* dev,
-		void (*process)(Packet* packet, void* context), void* context);
+		bool (*process)(Packet* packet, void* context), void* context);
 
 #endif /* __NICDEV_H__ */
