3458e232ed6cbe8d964c146676e8ab827b848d7d
sunghokim <sungho@gurum.cc>
2017-04-21 14:13:53 +0900

    manager use softlink

--- tools/manager/src/malloc.h
+++ tools/manager/src/malloc.h
@@ -1,11 +1,1 @@
-#ifndef __MALLOC_H__
-#define __MALLOC_H__
-
-#include <stdint.h>
-#include <sys/types.h>
-
-void malloc_init();
-size_t malloc_total();
-size_t malloc_used();
-
-#endif /* __MALLOC_H__ */
+../../../kernel/src/malloc.h
