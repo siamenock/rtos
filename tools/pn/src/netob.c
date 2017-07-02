4b80d9cf8b6d6161d3009fc71cda61374658dd95
makerj <ohenwkgdj@gmail.com>
2017-07-02 21:00:34 +0900

    Linux Network Interface State Observer (netob) is implemented

--- tools/pn/src/netob.c
+++ tools/pn/src/netob.c
@@ -1,269 +1,297 @@
-/*
-   (c) Jean Lorchat @ Internet Initiative Japan - Innovation Institute
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <endian.h>
 #include <sys/types.h>
+#include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <linux/netlink.h>
 #include <linux/rtnetlink.h>
 #include <linux/if_arp.h>
 #include <linux/limits.h>
-#include <util/event.h>
 
+
+#include <util/event.h>
+#include <net/interface.h>
+#include "netob.h"
 #include "driver/nicdev.h"
 #include "device.h"
 #include "dispatcher.h"
-#include "netob.h"
+#include "io_mux.h"
 
-#define IFLIST_REPLY_BUFFER	8192
-#define MAX_NET_DEVICES		8
+/// @see man 7 netlink
+/// @see man 7 rtnetlink
+/// @see man 7 netdevice
+/// @see https://tools.ietf.org/html/rfc3549
+/// @see http://people.netfilter.org/pablo/netlink/netlink.pdf
 
-/* PacketNgin wrapper struct for linux net_device */
-typedef struct nl_req_s nl_req_t;
 
-struct nl_req_s {
-	struct nlmsghdr hdr;
-	struct rtgenmsg gen;
-};
+static IOMultiplexer conn; ///< netob <--(netlink connection)-- linux kernel
+static int interfaces;
 
-static inline const char *ll_addr_n2a(const unsigned char *addr,
-		int alen, int type, char *buf, int blen) {
-	if(alen == 4 && (type == ARPHRD_TUNNEL || type == ARPHRD_SIT
-	     || type == ARPHRD_IPGRE)) {
-		return inet_ntop(AF_INET, addr, buf, blen);
-	}
-
-	if(alen == 16 && type == ARPHRD_TUNNEL6) {
-		return inet_ntop(AF_INET6, addr, buf, blen);
-	}
-
-	snprintf(buf, blen, "%02x", addr[0]);
-	for(int i = 1, l = 2; i < alen && l < blen; i++, l += 3)
-		snprintf(buf + l, blen - l, ":%02x", addr[i]);
-
-	return buf;
+static bool is_up(struct ifinfomsg* interface) {
+	return (interface->ifi_flags & IFF_UP) == IFF_UP;
 }
 
-static char* nlmsghdr_getname(struct nlmsghdr* h) {
-	if(!h) return NULL;
-	struct ifinfomsg *iface;
-	struct rtattr *attr;
-	int len;
-
-	iface = NLMSG_DATA(h);
-	len = h->nlmsg_len - NLMSG_LENGTH(sizeof(*iface));
-
-	for(attr= IFLA_RTA(iface); RTA_OK(attr, len); attr= RTA_NEXT(attr, len)) {
-		if(attr->rta_type == IFLA_IFNAME) {
-			return RTA_DATA(attr);
-		}
-	}
-	return NULL;
+static bool is_loopback(struct ifinfomsg* interface) {
+	return (interface->ifi_flags & IFF_LOOPBACK) == IFF_LOOPBACK;
 }
 
-static void rtnl_link(struct nlmsghdr *h, NICDevice* nicdev) {
-	struct ifinfomsg *iface;
-	struct rtattr *attribute;
-	int len;
+static bool typeclass(char* name, char* type) {
+	if(!name || !type) return false;
+	char classfile[PATH_MAX] = {};
+	snprintf(classfile, sizeof(classfile), "/sys/class/net/%s/%s", name, type);
 
-	iface = NLMSG_DATA(h);
-	len = h->nlmsg_len - NLMSG_LENGTH(sizeof(*iface));
+	return access(classfile, F_OK) == 0;
+}
 
-	for(attribute = IFLA_RTA(iface); RTA_OK(attribute, len);
-			attribute = RTA_NEXT(attribute, len)) {
-		switch(attribute->rta_type) {
+static bool is_bridge(char* name) {
+	return typeclass(name, "bridge");
+}
+
+static bool is_tap(char* name) {
+	return typeclass(name, "tun_flags");
+}
+
+static int on_newlink_event(struct nlmsghdr *h) {
+	struct ifinfomsg* info = NLMSG_DATA(h);
+	int len = h->nlmsg_len - NLMSG_LENGTH(sizeof(*info));
+	int error = 0;
+
+	char name[MAX_NIC_NAME_LEN] = {};
+	int mtu = 0;
+	uint64_t mac = 0;
+
+	for(struct rtattr* attr = IFLA_RTA(info); RTA_OK(attr, len); attr = RTA_NEXT(attr, len)) {
+		switch(attr->rta_type) {
 			case IFLA_IFNAME:
-				;
-				char* name = RTA_DATA(attribute);
-				strncpy(nicdev->name, name, MAX_NIC_NAME_LEN);
+				strncpy(name, RTA_DATA(attr), MAX_NIC_NAME_LEN);
 				break;
-
 			case IFLA_MTU:
-				;
-				int mtu = *(int *)RTA_DATA(attribute);
-				//device->status.mtu = mtu;
+				mtu = *(int *)RTA_DATA(attr);
 				break;
-
 			case IFLA_ADDRESS:
-				;
-				char buf[64];
-				uint8_t* mac = RTA_DATA(attribute);
-
-				for(int i = 0; i < ETH_ALEN; i++) {
-					nicdev->mac |=
-						(uint64_t)mac[i] << (ETH_ALEN - i - 1) * 8;
-				}
-
+				memcpy(&mac, RTA_DATA(attr), ETH_ALEN);
 				break;
-
 			default:
 				break;
 		}
 	}
-}
 
-static bool interface_up_check(struct ifinfomsg *iface) {
-	if((iface->ifi_flags & IFF_UP))
-		return true;
+	if(name[0] == '\0')/* || mtu == 0 || mac == 0) */
+		return 1;
+	if(is_loopback(info) || is_bridge(name) || is_tap(name)) // TODO Check aliased netif(eth0:0)
+		return 0;
 
-	return false;
-}
 
-static bool is_loopback(struct ifinfomsg *iface) {
-	return (iface->ifi_flags & IFF_LOOPBACK) == IFF_LOOPBACK;
-}
+	if(is_up(info)) {
+		NICDevice* nicdev = nicdev_get(name);
+		if(!nicdev) {
+			// Create NICDevice
+			nicdev = calloc(1, sizeof(NICDevice));
+			if(!nicdev) return 1;
+			strncpy(nicdev->name, name,  MAX_NIC_NAME_LEN);
+			nicdev->mtu = mtu;
+			nicdev->mac = mac;
 
-static bool is_bridge(char* name) {
-	if(!name) return false;
-	char bridge_config[PATH_MAX] = {};
-	snprintf(bridge_config, sizeof(bridge_config), "/sys/class/net/%s/bridge", name);
-
-	return access(bridge_config, F_OK) == 0;
-}
-
-static bool is_tap(char* name) {
-	if(!name) return false;
-	char tap_config[PATH_MAX] = {};
-	snprintf(tap_config, sizeof(tap_config), "/sys/class/net/%s/tun_flags", name);
-
-	return access(tap_config, F_OK) == 0;
-}
-
-bool netlink_event(void* context) {
-	int fd = (int)(uint64_t)context;
-	int len;
-	struct iovec io_reply;
-	struct sockaddr_nl kernel;
-	struct msghdr rtnl_reply;
-	char reply[IFLIST_REPLY_BUFFER];
-
-	memset(&io_reply, 0, sizeof(io_reply));
-	memset(&kernel, 0, sizeof(kernel));
-	memset(&rtnl_reply, 0, sizeof(rtnl_reply));
-
-	io_reply.iov_base = reply;
-	io_reply.iov_len = IFLIST_REPLY_BUFFER;
-
-	kernel.nl_family = AF_NETLINK;
-
-	rtnl_reply.msg_iov = &io_reply;
-	rtnl_reply.msg_iovlen = 1;
-	rtnl_reply.msg_name = &kernel;
-	rtnl_reply.msg_namelen = sizeof(kernel);
-
-	len = recvmsg(fd, &rtnl_reply, MSG_DONTWAIT);
-	if(len > 0) {
-		int error;
-		struct nlmsghdr *msg_ptr;
-		for(msg_ptr = (struct nlmsghdr *) reply; NLMSG_OK(msg_ptr, len); msg_ptr = NLMSG_NEXT(msg_ptr, len)) {
-			NICDevice* nicdev;
-			NICDevice* nicdev_internal;
-			struct ifinfomsg *iface;
-			iface = NLMSG_DATA(msg_ptr);
-
-			char* ifname = nlmsghdr_getname(msg_ptr);
-			if (is_loopback(iface) || is_bridge(ifname) || is_tap(ifname))
-				continue;
-
-			switch(msg_ptr->nlmsg_type) {
-				case NLMSG_DONE: /* NLMSG_DONE */
-					return true;
-					break;
-				case RTM_NEWLINK:
-					nicdev = malloc(sizeof(NICDevice));
-					memset(nicdev, 0, sizeof(NICDevice));
-					rtnl_link(msg_ptr, nicdev);
-					if(interface_up_check(iface)) {
-						// Register resources
-						if(!strncmp(nicdev->name, "v", 1)) { //Check VNIC
-							free(nicdev);
-							break;
-						}
-						error = nicdev_register(nicdev);
-						if(error) {
-							free(nicdev);
-							break;
-						}
-						error = dispatcher_create_nicdev(nicdev);
-						if(error) {
-							free(nicdev);
-							break;
-						}
-
-						printf("Register: %s\n", nicdev->name);
-					} else {
-						// Unregister resources
-						nicdev_internal = nicdev_unregister(nicdev->name);
-						if(!nicdev_internal) break;
-						error = dispatcher_destroy_nicdev(nicdev);
-						if(error) break;
-
-						printf("Unregister: %s\n", nicdev->name);
-						free(nicdev);
-						free(nicdev_internal);
-					}
-					break;
-				default:
-					break;
+			error = nicdev_register(nicdev);
+			if(error) {
+				free(nicdev);
+				return 1;
 			}
+			error = dispatcher_create_nicdev(nicdev);
+			if(error) {
+				nicdev_unregister(name);
+				free(nicdev);
+				return 1;
+			}
+
+			printf("netob: device '%s' is created\n", name);
+		} else {
+			// Update NICDevice
+			nicdev->mtu = mtu;
+			nicdev->mac = mac;
+			printf("netob: device '%s' is updated\n", name);
+		}
+
+	} else {
+		// Delete NICDevice
+		NICDevice* nicdev = nicdev_unregister(name);
+		if(!nicdev) return 0;
+		error = dispatcher_destroy_nicdev(nicdev);
+		if(error) return 1;
+
+		printf("netob: device '%s' is deleted\n", name);
+		free(nicdev);
+	}
+
+	return 0;
+}
+
+extern char* if_indextoname(unsigned int ifindex, char *ifname);
+static void on_ipv4_newroute_event(struct nlmsghdr* h) {
+	struct rtmsg* route = NLMSG_DATA(h);
+	if(route->rtm_table != RT_TABLE_MAIN) return;
+
+	int* ifindex = NULL;
+	char name[IFNAMSIZ];
+	uint32_t* address = NULL;
+	uint32_t netmask = htobe32(((~0) >> (32-route->rtm_dst_len)) << (32-route->rtm_dst_len));
+	uint32_t* gateway = NULL;
+
+
+	struct rtattr* attr = RTM_RTA(route);
+	int attrlen = RTM_PAYLOAD(h);
+	for(; RTA_OK(attr, attrlen); attr = RTA_NEXT(attr, attrlen)) {
+		void* value = RTA_DATA(attr);
+		switch(attr->rta_type) {
+			case RTA_DST:
+				break;
+			case RTA_GATEWAY:
+				gateway = value;
+				break;
+			case RTA_OIF:
+				ifindex = value;
+				break;
+			case RTA_PREFSRC:
+				address = value;
+				break;
+			default:
+				break;
 		}
 	}
 
-	return true;
+	if(!ifindex) return;
+	if(!is_tap(if_indextoname(*ifindex, name))) return;
+
+	NICDevice* nicdev = nicdev_get(name);
+	if(!nicdev) return;
+	VNIC* vnic = nicdev_get_vnic_name(nicdev, name);
+	if(!vnic) return;
+	NIC* nic = vnic->nic;
+
+	IPv4Interface* pnroute = interface_get_default(nic);
+	if(!pnroute) {
+		pnroute = interface_alloc(nic,
+				address ? *address: 0, netmask,
+				gateway ? *gateway : 0, true);
+	} else {
+		pnroute->netmask = netmask;
+		if(address)
+			pnroute->address = *address;
+		if(gateway)
+			pnroute->gateway = *gateway;
+	}
 }
 
-// FIXME: refactor netlink function
-int netob_init() {
-	int fd;
-	struct sockaddr_nl local;
-	struct sockaddr_nl kernel;
+static int observe(int fd, void* context) {
+	char response[32768] = {};
+	struct iovec response_vector = {response, sizeof(response)};
+	struct sockaddr_nl netlinkmsg = {AF_NETLINK, 0, 0, 0};
+	struct msghdr response_header = {
+		.msg_name = &netlinkmsg, .msg_namelen = sizeof(netlinkmsg),
+		.msg_iov = &response_vector, .msg_iovlen = 1,
+		NULL, 0, 0,
+	};
 
-	struct msghdr rtnl_msg;
-	struct iovec io;
+	ssize_t len = recvmsg(conn.fd, &response_header, 0);
+	if(len < 0) return -1;
 
-	nl_req_t req;
-
-	pid_t pid = getpid();
-
-	fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_ROUTE);
-
-	memset(&local, 0, sizeof(local));
-	local.nl_family = AF_NETLINK;
-	local.nl_pid = pid;
-	local.nl_groups = RTNLGRP_LINK;
-
-	if(bind(fd, (struct sockaddr *) &local, sizeof(local)) < 0) {
-		perror("Failed to bind rtnetlink socket\n");
-		return -1;
+	struct nlmsghdr* msg = (struct nlmsghdr *)response;
+	for(; NLMSG_OK(msg, len); msg = NLMSG_NEXT(msg, len)) {
+		switch(msg->nlmsg_type) {
+			case RTM_NEWLINK:
+				/*Add/Remove, Up/Down, MAC, MTU, Rx/Tx Queue*/
+				on_newlink_event(msg);
+				break;
+			case RTM_NEWADDR:
+				/*IP, Run, Power Management*/
+				break;
+			case RTM_NEWROUTE:
+				/*IP, Netmask, Gateway, Route*/
+				on_ipv4_newroute_event(msg);
+				break;
+			case NLMSG_DONE:
+				/*No more message*/
+				return 0;
+				break;
+			default:
+				break;
+		}
 	}
 
-	memset(&rtnl_msg, 0, sizeof(rtnl_msg));
-	memset(&kernel, 0, sizeof(kernel));
-	memset(&req, 0, sizeof(req));
+	return 0;
+}
 
-	kernel.nl_family = AF_NETLINK;
+static int request_scan_all_interfaces(int socket) {
+	int error = 0;
+	struct iovec io = {}; // request array
+	struct nlmsghdr req = {}; // request content
+	{
+		req.nlmsg_len = NLMSG_LENGTH(sizeof(struct rtgenmsg));
+		req.nlmsg_type = RTM_GETLINK;
+		req.nlmsg_flags = NLM_F_REQUEST | NLM_F_ATOMIC | NLM_F_DUMP;
+		req.nlmsg_seq = 1;
+		req.nlmsg_pid = getpid();
+		io.iov_base = &req;
+		io.iov_len = req.nlmsg_len;
+	}
+	struct msghdr rtnl_msg = {}; // reqeust object(header + content)
+	struct sockaddr_nl kernel = {};
+	{
+		kernel.nl_family = AF_NETLINK;
+		rtnl_msg.msg_iov = &io;
+		rtnl_msg.msg_iovlen = 1;
+		rtnl_msg.msg_name = &kernel;
+		rtnl_msg.msg_namelen = sizeof(kernel);
+	}
 
-	req.hdr.nlmsg_len = NLMSG_LENGTH(sizeof(struct rtgenmsg));
-	req.hdr.nlmsg_type = RTM_GETLINK;
-	req.hdr.nlmsg_flags = NLM_F_REQUEST | NLM_F_ATOMIC | NLM_F_DUMP;
-	req.hdr.nlmsg_seq = 1;
-	req.hdr.nlmsg_pid = pid;
-	req.gen.rtgen_family = AF_PACKET;
+	// Send request
+	error = (int)sendmsg(socket, (struct msghdr *) &rtnl_msg, 0);
+	if(error <= 0) return 3;
 
-	io.iov_base = &req;
-	io.iov_len = req.hdr.nlmsg_len;
-	rtnl_msg.msg_iov = &io;
-	rtnl_msg.msg_iovlen = 1;
-	rtnl_msg.msg_name = &kernel;
-	rtnl_msg.msg_namelen = sizeof(kernel);
+	return error;
+}
 
-	sendmsg(fd, (struct msghdr *) &rtnl_msg, 0);
-	event_idle_add(netlink_event, (void*)(uint64_t)fd);
+int netob_init() {
+	int error = 0;
 
+	// Create network interface query socket for metadata queries
+	interfaces = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
+	if(interfaces == -1) return 1;
+
+	// Specify netlink channel
+	struct sockaddr_nl channel = {
+		.nl_family = AF_NETLINK,
+		.nl_pid = getpid(),
+		.nl_groups = RTMGRP_LINK | RTMGRP_IPV4_IFADDR | RTMGRP_IPV4_ROUTE,
+	};
+
+	// Bind
+	conn.fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_ROUTE);
+	if(conn.fd == -1) return 2;
+	error = bind(conn.fd, (struct sockaddr *) &channel, sizeof(channel));
+	if(error) return 3;
+
+	// Send initial scan reqeust
+	error = request_scan_all_interfaces(conn.fd);
+	if(error) return 4;
+
+	// Listen network interface status events
+	conn.read_handler = observe;
+	error = (int)io_mux_add(&conn, (uint64_t)&conn);
+	if(error == 0) return 5;
+
+	return 0;
+}
+
+int netob_fini() {
+	io_mux_remove((uint64_t)&conn);
+	if(conn.fd > 0)
+		close(conn.fd);
+	if(interfaces > 0)
+		close(interfaces);
 	return 0;
 }
--- tools/pn/src/netob.h
+++ tools/pn/src/netob.h
@@ -1,6 +1,33 @@
 #ifndef __NETOB_H__
 #define __NETOB_H__
 
+/**
+ * @file
+ * Network Interface Observer (netob)
+ *
+ * This module provides the following functions using netlink
+ * - Scan all of existing linux network interfaces
+ * - Event listening
+ *   - Netowrk interface up/down event
+ *   - Link level configuration modification event
+ *   - IP configuration modification event
+ *
+ * Events are delivered as shown in the following sequence:
+ * linux kernel --(netlink connection)--> netob --(shared memory)--> packetngin
+ */
+
+/**
+ * Initialize module and start observing
+ *
+ * @return zero for success, nonzero for failure
+ */
 int netob_init();
+
+/**
+ * Finalize module
+ *
+ * @return zero for success, nonzero for failure
+ */
+int netob_fini();
 
 #endif // __NETOB_H__
