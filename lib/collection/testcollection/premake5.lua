949c38e1ac0550cfe819aaf6779e268d10d329cd
makerj <ohenwkgdj@gmail.com>
2017-07-05 19:12:50 +0900

    Add missing compile option

--- lib/collection/testcollection/premake5.lua
+++ lib/collection/testcollection/premake5.lua
@@ -7,4 +7,4 @@ project 'testcollection'
 	files { "src/**.h", "src/**.c" }
     links { 'collection' }
 	postbuildcommands { "%{cfg.buildtarget.abspath}" } -- run tester
-    buildoptions    '-fms-extensions'
+    buildoptions { '-fms-extensions', '-std=gnu11' }
